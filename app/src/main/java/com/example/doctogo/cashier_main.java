e93531093804aeb5ae7f95734107c2cc5f8972a5
easyyliu <easyyliu@gmail.com>
2020-04-04 18:56:54 -0700

    cashier edit

--- app/src/main/java/com/example/doctogo/cashier_main.java
+++ app/src/main/java/com/example/doctogo/cashier_main.java
@@ -5,22 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class cashier_main extends AppCompatActivity {
-
+    private String cashier_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cashier_main);
+        TextView name_cashier = (TextView)findViewById(R.id.name_cashier);
         Button btn_newTrans = (Button)findViewById(R.id.btn_cashier_addNewTransaction);
         Button btn_viewTrans = (Button)findViewById(R.id.btn_cashier_viewTransactions);
         Button btn_archTrans = (Button)findViewById(R.id.btn_cashier_archiveTransaction);
         Button btn_logout = (Button)findViewById(R.id.btn_cashier_LogOut);
         final SharedPreferences storage = getSharedPreferences("DOCTOGOSESSION", Context.MODE_PRIVATE);
         int userID = storage.getInt("USERID",0);
+        DatabaseHelper dbh = new DatabaseHelper(this);
+        Cursor c = dbh.getInformationUser(userID);
+        if (c.getCount() > 0) {
+            while (c.moveToNext()) {
+                cashier_name = "Hi, " + c.getString(4) + " " + c.getString(5);
+                name_cashier.setText(cashier_name);
+            }
+        }
         btn_newTrans.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
